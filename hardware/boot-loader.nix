0d15970ce3304dd728bfaa57c3cfe24b6f390258
Naharie <oneofthosewhoescaped@gmail.com>
2023-11-18 22:12:42 -0700

    Add kde-wallpaper-engine-backend to packages

--- hardware/boot-loader.nix
+++ hardware/boot-loader.nix
@@ -3,7 +3,9 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  #boot.kernelModules = [ "i2c-dev" ];
+
+  # Before using this check if hardware.module-name.enable exists.
+  #boot.kernelModules = [ "kernel-module-goes-here" ];
 
   # Extra flags like the one Isaiah needs.
   # boot.kernelParams = [ "no-aer" ]
