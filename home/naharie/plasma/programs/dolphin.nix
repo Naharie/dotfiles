61fac95999012e320aec671542137a9460d8c8fe
Naharie <naharie@pm.me>
2025-01-04 23:17:57 -0700

    Fix dolphin config

--- home/naharie/plasma/programs/dolphin.nix
+++ home/naharie/plasma/programs/dolphin.nix
@@ -44,60 +44,58 @@ in
       ];
     };
 
-    configFile = {
-      kdeglobals = {
-        KDE = {
-          SingleClick = false;
-          ShowDeleteCommand = true;
-        };
-
-        PreviewSettings = {
-          EnableRemoteFolderThumbnail = true;
-          MaximumRemoteSize = 31457280;
-        };
-
-        "KFileDialog Settings" = {
-          "Automatically select filename extension" = true;
-          "Show Full Path" = true;
-          "Show Inline Previews" = true;
-          "Show Preview" = false;
-          "Show hidden files" = false;
-          "Sort by" = "Name";
-          "Sort directories first" = true;
-        };
+    kdeglobals = {
+      KDE = {
+        SingleClick = false;
+        ShowDeleteCommand = true;
       };
 
-      kiorc = {
-        Confirmations = {
-          ConfirmDelete = true;
-          ConfirmEmptyTrash = true;
-          ConfirmTrash = false;
-        };
-
-        "Executable scripts".behaviourOnLaunch = "alwaysAsk";
+      PreviewSettings = {
+        EnableRemoteFolderThumbnail = true;
+        MaximumRemoteSize = 31457280;
       };
 
-      kservicemenurc.Show = {
-        compressfileitemaction = true;
-        extractfileitemaction = true;
-        forgetfileitemaction = true;
-        installFont = true;
-        kactivitymanagerd_fileitem_linking_plugin = true;
-        kio-admin = true;
-        makefileactions = true;
-        mountisoaction = true;
-        movetonewfolderitemaction = true;
-        runInKonsole = true;
-        slideshowfileitemaction = true;
-        tagsfileitemaction = true;
-        wallpaperfileitemaction = true;
+      "KFileDialog Settings" = {
+        "Automatically select filename extension" = true;
+        "Show Full Path" = true;
+        "Show Inline Previews" = true;
+        "Show Preview" = false;
+        "Show hidden files" = false;
+        "Sort by" = "Name";
+        "Sort directories first" = true;
+      };
+    };
+
+    kiorc = {
+      Confirmations = {
+        ConfirmDelete = true;
+        ConfirmEmptyTrash = true;
+        ConfirmTrash = false;
       };
 
-      ktrashrc = {
-        "\\/home\\/naharie\\/.local\\/share\\/Trash" = {
-          UseSizeLimit = false;
-          UseTimeLimit = false;
-        };
+      "Executable scripts".behaviourOnLaunch = "alwaysAsk";
+    };
+
+    kservicemenurc.Show = {
+      compressfileitemaction = true;
+      extractfileitemaction = true;
+      forgetfileitemaction = true;
+      installFont = true;
+      kactivitymanagerd_fileitem_linking_plugin = true;
+      kio-admin = true;
+      makefileactions = true;
+      mountisoaction = true;
+      movetonewfolderitemaction = true;
+      runInKonsole = true;
+      slideshowfileitemaction = true;
+      tagsfileitemaction = true;
+      wallpaperfileitemaction = true;
+    };
+
+    ktrashrc = {
+      "\\/home\\/naharie\\/.local\\/share\\/Trash" = {
+        UseSizeLimit = false;
+        UseTimeLimit = false;
       };
     };
   };
