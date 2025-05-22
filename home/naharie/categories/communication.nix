d6984329f77a6e6cb3ccd30068eadfe2dc9fdcc6
Naharie <naharie@pm.me>
2025-05-22 01:51:34 -0600

    Drop skype and localsend

--- home/naharie/categories/communication.nix
+++ home/naharie/categories/communication.nix
@@ -6,8 +6,6 @@
   home.packages = with pkgs; [
     webcord # Discord client
     vesktop # Discord client for streaming
-    skypeforlinux
     signal-desktop
-    localsend # Share files with phone and tablet
   ];
 }
