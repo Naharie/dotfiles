b33fdfb637a4f9ce329343faf2819cf85f2fdd9c
Naharie <naharie@pm.me>
2025-05-29 23:04:53 -0600

    Update zrok to version 1.0.4

--- home/naharie/programs/zrok.nix
+++ home/naharie/programs/zrok.nix
@@ -3,7 +3,17 @@
 }:
 
 {
-  home.packages = with pkgs; [
-    zrok
+  home.packages = [
+    (let version = "1.0.4"; in
+
+    pkgs.zrok.overrideAttrs (finalAttrs: previousAttrs: {
+      inherit version;
+
+      src = pkgs.fetchzip {
+        url = "https://github.com/openziti/zrok/releases/download/v${version}/zrok_${version}_linux_amd64.tar.gz";
+        stripRoot = false;
+        hash = "sha256-Ewez2QUsIAmxyjxR8wvt7UJpXVHjIb8s6gGF1YNgrec=";
+      };
+    }))
   ];
 }
