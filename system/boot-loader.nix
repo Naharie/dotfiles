2c92fb506da30bd65eed4b1fe7fc8290461f4621
Naharie <naharie@pm.me>
2025-07-16 19:42:20 -0600

    Add boot entry for Windows

--- system/boot-loader.nix
+++ system/boot-loader.nix
@@ -2,7 +2,16 @@
 }:
 
 {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    windows = {
+      "11" = {
+        title = "Windows 11";
+        efiDeviceHandle = "M2_2";
+        sortKey = "z_windows_11";
+      };
+    };
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   # For MakeMKV to detect the optical drive
