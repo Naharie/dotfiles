4aa6457be9e20613457fc842a480dfdcb87f5772
Naharie <naharie@pm.me>
2025-07-17 19:45:17 -0600

    Patch the kernel to allow for async reprojection in Steam VR

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -15,4 +15,25 @@
   };
 
   environment.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${pkgs.proton-ge-custom}";
+
+  hardware.steam-hardware.enable = true;
+
+  boot = {
+    kernel.sysctl = {
+      # SteamOS/Fedora default, can help with performance.
+      "vm.max_map_count" = 2147483642;
+    };
+
+    # For asynchronous reprojection support in Steam VR
+    kernelPatches = [
+      {
+        name = "amdgpu-ignore-ctx-privileges";
+        patch = pkgs.fetchpatch {
+          name = "cap_sys_nice_begone.patch";
+          url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+          hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+        };
+      }
+    ];
+  };
 }
